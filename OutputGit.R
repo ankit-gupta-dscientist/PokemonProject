2e9752ba13794dbcacc71148027836db919e80f1
ankit-gupta-dscientist <44063251+ankit-gupta-dscientist@users.noreply.github.com>
2023-10-11 15:36:45 -0400

    Update OutputGit.R

--- OutputGit.R
+++ OutputGit.R
@@ -2,7 +2,7 @@
 library(rmarkdown)
 
 # Define the path to your R Markdown file
-rmarkdown_file <- "Project2_Gupta_PokeAPI.Rmd"  # Replace with your actual file name
+rmarkdown_file <- "Project2_Gupta_PokeAPI.Rmd" 
 
 # Render the R Markdown to README.md
 rmarkdown::render(input = rmarkdown_file, output_format = "github_document")
