b75fb1975e19478d7fb1e1b5e30396ec4d46f931
ankit-gupta-dscientist <44063251+ankit-gupta-dscientist@users.noreply.github.com>
2023-10-11 15:24:37 -0400

    Add files via upload

---
+++ OutputGit.R
@@ -0,0 +1,8 @@
+# Load the rmarkdown package
+library(rmarkdown)
+
+# Define the path to your R Markdown file
+rmarkdown_file <- "Project2_Gupta_PokeAPI.Rmd"  # Replace with your actual file name
+
+# Render the R Markdown to README.md
+rmarkdown::render(input = rmarkdown_file, output_format = "github_document")
